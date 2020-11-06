751a4f86d3b0efe76720d1fa53de34ab2b25c2b7
flynn.huang <hc092783@gmail.com>
2020-11-06 15:53:23 +0800

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:xtools/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Profit route test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(XToolApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.byIcon(FontAwesomeIcons.angleDoubleRight));
+    await tester.pumpAndSettle();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('计算收益'), findsOneWidget);
   });
 }
